6b091d2feda6c8d3108c94198aa6ee3297b75edb
chutchais <chutchai.s@gmail.com>
2020-10-19 10:30:33 +0700

    Add BL link on container report

--- src/bl/urls.py
+++ src/bl/urls.py
@@ -5,6 +5,7 @@ from .views import index,container,get_bl_info,get_container_info
 urlpatterns = [
     path('', index, name='home'),
     path('report',index,name='report'),
+    path('report/<bl>',index,name='report-bl'),
     path('container',container,name='container'),
     path('api/bl/<bl>',get_bl_info,name='bl'),
     path('api/container/<container>',get_container_info,name='container'),
--- src/bl/views.py
+++ src/bl/views.py
@@ -4,12 +4,13 @@ from django.urls import reverse_lazy
 from django.http import JsonResponse
 
 # Create your views here.
-def index(request):
+def index(request,bl=''):
 	context = {}
 	bl_url = f"{reverse_lazy('bl:home')}"
 	# print(bl_url)
 		# print (f"{reverse_lazy('order:list')}booking/")
 	context['import_bl_url'] = f'{bl_url}api/bl/'
+	context['bl'] = bl
 	return render(request, 'bl/index.html', context=context)
 
 def container(request):
@@ -22,24 +23,24 @@ def container(request):
 
 
 def get_bl_info(request,bl):
-    import json
-    http = urllib3.PoolManager()
-    # print ('get_booking_info')
-    r = http.request('GET', f'http://192.168.10.16:5000/bl/{bl}')
-    # return json.loads(r.data.decode('utf-8'))
-    response = JsonResponse(json.loads(r.data.decode('utf-8')), safe=False)
-    response['Access-Control-Allow-Origin'] = '*'
-    response['Access-Control-Allow-Headers'] = '*'
-    return response
+	import json
+	http = urllib3.PoolManager()
+	# print ('get_booking_info')
+	r = http.request('GET', f'http://192.168.10.16:5000/bl/{bl}')
+	# return json.loads(r.data.decode('utf-8'))
+	response = JsonResponse(json.loads(r.data.decode('utf-8')), safe=False)
+	response['Access-Control-Allow-Origin'] = '*'
+	response['Access-Control-Allow-Headers'] = '*'
+	return response
 
 # Added on Oct 16,2020 -- To support Import container info API
 def get_container_info(request,container):
-    import json
-    http = urllib3.PoolManager()
-    # print ('get_booking_info')
-    r = http.request('GET', f'http://192.168.10.16:5000/container/{container}')
-    # return json.loads(r.data.decode('utf-8'))
-    response = JsonResponse(json.loads(r.data.decode('utf-8')), safe=False)
-    response['Access-Control-Allow-Origin'] = '*'
-    response['Access-Control-Allow-Headers'] = '*'
-    return response
+	import json
+	http = urllib3.PoolManager()
+	# print ('get_booking_info')
+	r = http.request('GET', f'http://192.168.10.16:5000/container/{container}')
+	# return json.loads(r.data.decode('utf-8'))
+	response = JsonResponse(json.loads(r.data.decode('utf-8')), safe=False)
+	response['Access-Control-Allow-Origin'] = '*'
+	response['Access-Control-Allow-Headers'] = '*'
+	return response
