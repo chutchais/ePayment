07a870b6ba32a7c414db0d646dcc3faf34afc733
chutchais <chutchai.s@gmail.com>
2020-11-18 17:15:03 +0700

    Remove pagination from address API

--- src/user_profile/api/views.py
+++ src/user_profile/api/views.py
@@ -6,6 +6,8 @@ from .serializers import AddressSerializer
 class AddressList(generics.ListAPIView):
 	queryset = Address.objects.filter(status=True)
 	serializer_class = AddressSerializer
+	pagination_class = None
+	
 	def get_queryset(self):
 		current_user = self.request.user
 		return Address.objects.filter(user=current_user,status=True).order_by('company')
