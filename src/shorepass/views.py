c945bce91d63bf626cc672446d6b9667f8052802
chutchais <chutchai.s@gmail.com>
2020-11-26 09:58:18 +0700

    Remove permission out from shorepass function

--- src/shorepass/views.py
+++ src/shorepass/views.py
@@ -127,11 +127,12 @@ class ShoreDeleteView(LoginRequiredMixin,DeleteView):
         else:
             raise Http404("Not allow to delete") #or return HttpResponse('404_url')
 
-class ShoreUpdateExecuteJob(PermissionRequiredMixin,LoginRequiredMixin,UpdateView):
+
+class ShoreUpdateExecuteJob(LoginRequiredMixin,UpdateView):
     model = Shore
     fields = ['execute_job']
     template_name_suffix = '_update_executejob_form'
-    permission_required = ('shorepass.verify_shore', 'shorepass.execute_job')
+    # permission_required = ('shore.verify_shore', 'shore.execute_job')
 
     def form_valid(self, form):
         import datetime, pytz
@@ -141,8 +142,8 @@ class ShoreUpdateExecuteJob(PermissionRequiredMixin,LoginRequiredMixin,UpdateVie
         form.instance.execute_by = self.request.user
         return super(ShoreUpdateExecuteJob, self).form_valid(form)
 
-class ShoreUpdateContactMessage(PermissionRequiredMixin,LoginRequiredMixin,UpdateView):
+class ShoreUpdateContactMessage(LoginRequiredMixin,UpdateView):
     model = Shore
     fields = ['need_contact','message']
     template_name_suffix = '_update_message_form'
-    permission_required = ('shorepass.verify_shore', 'shorepass.execute_job')
+    # permission_required = ('shorepass.verify_shore', 'shorepass.execute_job')
