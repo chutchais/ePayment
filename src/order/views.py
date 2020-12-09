2f6319fcaf67af098349e513b376e39fc72bb7d0
chutchais <chutchai.s@gmail.com>
2020-12-09 15:31:48 +0700

    Change order limit to 700

--- src/order/views.py
+++ src/order/views.py
@@ -193,7 +193,7 @@ class OrderListView(LoginRequiredMixin,ListView):
                                         user__username=self.request.user ).select_related('booking').order_by('-updated')[:100]
                                         # Q(booking__name__icontains=query) ,-->Removed for optimize
         if self.request.user.has_perm('order.verify_payment') or  self.request.user.has_perm('order.update_payment') :
-            return Order.objects.all().select_related('booking').order_by('-updated')[:300]
+            return Order.objects.all().select_related('booking').order_by('-updated')[:700]
 
         return Order.objects.filter(user__username=self.request.user).select_related('booking').order_by('-updated')[:100]
     
